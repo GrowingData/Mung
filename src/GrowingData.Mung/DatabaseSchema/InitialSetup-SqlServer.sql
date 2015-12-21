26d74d4d9b3158fcb8107efc94a09e95e0c3f2f6
Terence Siganakis <terence@siganakis.com>
2015-12-22 08:45:22 +1100

    More on reporting front end

--- src/GrowingData.Mung/DatabaseSchema/InitialSetup-SqlServer.sql
+++ src/GrowingData.Mung/DatabaseSchema/InitialSetup-SqlServer.sql
@@ -2,14 +2,10 @@
 CREATE SCHEMA mung
 
 
-DROP TABLE mung.Component 
+DROP TABLE mung.Graph 
 DROP TABLE mung.Dashboard 
 DROP TABLE mung.Munger
 GO
-
-DELETE FROM mung.Component
-
-SELECT * FROM mung.Component
 
 CREATE TABLE mung.Munger(
 	MungerId [int] IDENTITY(1,1) NOT NULL,
@@ -27,10 +23,11 @@ CREATE TABLE mung.Munger(
 
 GO
 
+
 CREATE TABLE mung.Dashboard (
 	DashboardId INT NOT NULL IDENTITY(1,1),
 	Url VARCHAR(256) NOT NULL,
-	Title VARCHAR(256) NULL,
+	Title VARCHAR(256) NOT NULL,
 	Css NVARCHAR(MAX) NULL,
 	CreatedAt DATETIME NOT NULL,
 	UpdatedAt DATETIME NOT NULL,
@@ -41,18 +38,19 @@ CREATE TABLE mung.Dashboard (
 	CONSTRAINT UQ_Dashboard_Url UNIQUE (Url)
 )
 
-CREATE TABLE mung.Component (
-	ComponentId INT NOT NULL IDENTITY(1,1),
+CREATE TABLE mung.Graph (
+	GraphId INT NOT NULL IDENTITY(1,1),
 	DashboardId INT NOT NULL,
+	Title NVARCHAR(MAX) NOT NULL,
 	Html NVARCHAR(MAX),
 	Sql NVARCHAR(MAX),
 	Js NVARCHAR(MAX) NULL,
-	PositionX FLOAT NOT NULL,
-	PositionY FLOAT NOT NULL,
+	X FLOAT NOT NULL,
+	Y FLOAT NOT NULL,
 	Width FLOAT NOT NULL,
 	Height FLOAT NOT NULL
 
-	CONSTRAINT PK_Component PRIMARY KEY (ComponentId),
-	CONSTRAINT FK_Component_Dashboard FOREIGN KEY (DashboardId) REFERENCES mung.Dashboard(DashboardId)
+	CONSTRAINT PK_Graph PRIMARY KEY (GraphId),
+	CONSTRAINT FK_Graph_Dashboard FOREIGN KEY (DashboardId) REFERENCES mung.Dashboard(DashboardId)
 )
 GO
